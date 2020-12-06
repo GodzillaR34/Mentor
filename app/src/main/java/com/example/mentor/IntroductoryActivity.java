4ae47e287d729520b95a945716b93ad3fa6254fa
Wilder-reyes <wilder25022004@gmail.com>
2020-12-05 23:26:43 -0500

    code

---
+++ app/src/main/java/com/example/mentor/IntroductoryActivity.java
@@ -0,0 +1,29 @@
+package com.example.mentor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+
+public class IntroductoryActivity extends AppCompatActivity {
+    Imageview logo, appName, splashImg;
+    LottieAnimationView lottieAnimationView;
+
+    @SuppressLint("WrongViewCast")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_introductory);
+
+        logo = findViewById(R.id.logo);
+        appName = findViewById(R.id.app_name);
+        splashImg = findViewById(R.id.img);
+        lottieAnimationView = findViewById(R.id.lottie);
+
+        splashImg.animate().translationY(-1600).setDuration(1000).setStartDelay(4000);
+        logo.animate().translationY(1400).setDuration(1000).setStartDelay(4000);
+        appName.animate().translationY(1400).setDuration(1000).setStartDelay(4000);
+        lottieAnimationView.animate().translationY(1600).setDuration(1000).setStartDelay(4000);
+    }
+}
+
